af3d9744027544b09cc88d6ff0e006ad941e45cd
bbrodo <15292@coderacademy.edu.au>
2024-09-23 11:43:10 +1000

    feat: react app init

--- jsclasses/app.js
+++ jsclasses/app.js
@@ -7,8 +7,20 @@ class Dog {
         this.cuteness = newCuteness;
 
     }
+
+    bark(){
+        console.log("Woof! i am a " + this.breed)
+    }
 }
 
 let fido = new Dog("Fido", "Frenchie", "leather", 7);
 
 console.log(fido);
+
+fido.bark()
+
+class BarkError extends Error {
+    constructor(dogName, dogBreed){
+        super(`${dogName} of breed ${dogBreed} failed to bark.`)
+    }
+}
